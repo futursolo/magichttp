e0d3593cd3b4c5ac96b07a88e5b7c04904c461c3
星川かえで <futursolo@icloud.com>
2018-01-01 15:41:59 -0500

    2018 Happy New Year!

--- README.rst
+++ README.rst
@@ -20,7 +20,7 @@ Use at your own risk.
 
 License
 -------
-Copyright 2017 Kaede Hoshikawa
+Copyright 2018 Kaede Hoshikawa
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- tests/test_h1composers.py
+++ tests/test_h1composers.py
@@ -99,3 +99,106 @@ class ComposeResponseInitialTestCase:
             b"Server: magichttp/%s\r\n" % __version__.encode() +
             b"Connection: Close\r\n" +
             b"Transfer-Encoding: Chunked\r\n\r\n")
+
+    def test_bad_request(self):
+        res, res_bytes = compose_response_initial(
+            http.HTTPStatus.BAD_REQUEST, headers=None, req_initial=None)
+
+        assert res.status_code == 400
+        assert res.headers == {
+            b"server": b"magichttp/%s" % __version__.encode(),
+            b"connection": b"Close",
+            b"transfer-encoding": b"Chunked"}
+
+        assert res_bytes == (
+            b"HTTP/1.1 400 Bad Request\r\n" +
+            b"Server: magichttp/%s\r\n" % __version__.encode() +
+            b"Connection: Close\r\n" +
+            b"Transfer-Encoding: Chunked\r\n\r\n")
+
+    def test_http_10(self):
+        req = HttpRequestInitial(
+            HttpRequestMethod.Get,
+            version=HttpVersion.V1_0,
+            uri=b"/",
+            scheme=b"http",
+            headers=magicdict.TolerantMagicDict(),
+            authority=None)
+
+        res, res_bytes = compose_response_initial(
+            http.HTTPStatus.OK, headers=None, req_initial=req)
+
+        assert res.status_code == 200
+        assert res.headers == {
+            b"server": b"magichttp/%s" % __version__.encode(),
+            b"connection": b"Close"}
+
+        assert res_bytes == (
+            b"HTTP/1.0 200 OK\r\n" +
+            b"Server: magichttp/%s\r\n" % __version__.encode() +
+            b"Connection: Close\r\n\r\n")
+
+    def test_keep_alive(self):
+        req = HttpRequestInitial(
+            HttpRequestMethod.Get,
+            version=HttpVersion.V1_1,
+            uri=b"/",
+            scheme=b"http",
+            headers=magicdict.TolerantMagicDict(
+                [(b"connection", b"Keep-Alive")]),
+            authority=None)
+
+        res, res_bytes = compose_response_initial(
+            http.HTTPStatus.OK, headers=None, req_initial=req)
+
+        assert res.status_code == 200
+        assert res.headers == {
+            b"server": b"magichttp/%s" % __version__.encode(),
+            b"connection": b"Keep-Alive",
+            b"transfer-encoding": b"Chunked"}
+
+        assert res_bytes == (
+            b"HTTP/1.1 200 OK\r\n" +
+            b"Server: magichttp/%s\r\n" % __version__.encode() +
+            b"Connection: Keep-Alive\r\n" +
+            b"Transfer-Encoding: Chunked\r\n\r\n")
+
+    def test_no_keep_alive(self):
+        req = HttpRequestInitial(
+            HttpRequestMethod.Get,
+            version=HttpVersion.V1_1,
+            uri=b"/",
+            scheme=b"http",
+            headers=magicdict.TolerantMagicDict(
+                [(b"connection", b"Close")]),
+            authority=None)
+
+        res, res_bytes = compose_response_initial(
+            http.HTTPStatus.OK, headers=None, req_initial=req)
+
+        assert res.status_code == 200
+        assert res.headers == {
+            b"server": b"magichttp/%s" % __version__.encode(),
+            b"connection": b"Close",
+            b"transfer-encoding": b"Chunked"}
+
+        assert res_bytes == (
+            b"HTTP/1.1 200 OK\r\n" +
+            b"Server: magichttp/%s\r\n" % __version__.encode() +
+            b"Connection: Close\r\n" +
+            b"Transfer-Encoding: Chunked\r\n\r\n")
+
+
+class ComposeChunkedBodyTestCase:
+    def test_normal_chunk(self):
+        assert compose_chunked_body(b"a") == b"1\r\na\r\n"
+
+    def test_empty_chunk(self):
+        assert compose_chunked_body(b"") == b""
+
+    def test_normal_last_chunk(self):
+        assert compose_chunked_body(b"a", finished=True) == \
+            b"1\r\na\r\n0\r\n\r\n"
+
+    def test_empty_last_chunk(self):
+        assert compose_chunked_body(b"", finished=True) == b"0\r\n\r\n"
